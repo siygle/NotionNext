df6c0f0e769053281752f3a0b505869ab3fd44e5
sy <shiyung@gmail.com>
2023-04-30 17:34:08 +0800

    chore: show up RSS btn

--- themes/nobelium/config_nobelium.js
+++ themes/nobelium/config_nobelium.js
@@ -5,7 +5,7 @@ const CONFIG_NOBELIUM = {
   MENU_TAG: true, // 显示标签
   MENU_ARCHIVE: false, // 显示归档
   MENU_SEARCH: true, // 显示搜索
-  MENU_RSS: false, // 显示订阅
+  MENU_RSS: true, // 显示订阅
 
   NAV_NOTION_ICON: true // 是否读取Notion图标作为站点头像
 
